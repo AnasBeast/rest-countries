8c7838366e4f4d59007c7c31901c342b96266cc1
AnasBeast <boussehminea@gmail.com>
2024-12-16 23:59:28 +0100

    Refactor CountryPage to use Suspense for loading state and extract CountryInfoWrapper component

--- src/app/country/page.tsx
+++ src/app/country/page.tsx
@@ -1,7 +1,6 @@
 'use client'
-import React, { useEffect, useState } from 'react';
+import React, { Suspense } from 'react';
 import CountryInfos from './Country';
-import { useRouter } from 'next/router';
 import { useSearchParams } from 'next/navigation';
 import { useTheme } from '../Context/ThemeContext';
 
@@ -14,17 +13,24 @@ interface Country {
 }
 
 const CountryPage: React.FC = () => {
-  const searchParams = useSearchParams();
-  const countryName = searchParams.get('name') || '';
-  const {theme} = useTheme();
-  
+  const { theme } = useTheme();
+
   return (
-    <div className={`w-full sm:h-screen overflow-y-hidden ${theme==='dark'?"bg-background":"bg-lightback"}`}>
-      <div className={`w-10/12 mx-auto py-10 `}>
-      <CountryInfos countryName={countryName} />
+    <div className={`w-full sm:h-screen overflow-y-hidden ${theme === 'dark' ? "bg-background" : "bg-lightback"}`}>
+      <div className={`w-10/12 mx-auto py-10`}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <CountryInfoWrapper />
+        </Suspense>
       </div>
     </div>
   );
+};
+
+const CountryInfoWrapper: React.FC = () => {
+  const searchParams = useSearchParams();
+  const countryName = searchParams.get('name') || '';
+
+  return <CountryInfos countryName={countryName} />;
 };
 
 export default CountryPage;
